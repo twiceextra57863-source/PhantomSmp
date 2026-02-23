82c66a418cd76a6f7f2aa7fa411f00c05ffb0c7c
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-23 14:24:56 +0530

    Update SMPStarter.java

--- src/main/java/com/yourname/smpstarter/SMPStarter.java
+++ src/main/java/com/yourname/smpstarter/SMPStarter.java
@@ -6,37 +6,49 @@ import com.yourname.smpstarter.commands.SMPStartCommand;
 import com.yourname.smpstarter.listeners.BookListener;
 import com.yourname.smpstarter.listeners.ProtectionListener;
 import com.yourname.smpstarter.manager.BookManager;
-import com.yourname.smpstarter.manager.ParticleManager;
 import com.yourname.smpstarter.manager.TimerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SMPStarter extends JavaPlugin {
-
+    
+    private static SMPStarter instance;
     private TimerManager timerManager;
     private BookManager bookManager;
-    private ParticleManager particleManager;
-
+    
     @Override
     public void onEnable() {
+        instance = this;
+        
         // Initialize managers
         timerManager = new TimerManager(this);
-        bookManager = new BookManager();
-        particleManager = new ParticleManager();
-
+        bookManager = new BookManager(this);
+        
         // Register commands
-        getCommand("smpstart").setExecutor(new SMPStartCommand(timerManager));
-        getCommand("givebook").setExecutor(new GiveBookCommand(bookManager));
-        getCommand("randombook").setExecutor(new RandomBookCommand(bookManager));
-
+        getCommand("smpstart").setExecutor(new SMPStartCommand(this));
+        getCommand("givebook").setExecutor(new GiveBookCommand(this));
+        getCommand("randombook").setExecutor(new RandomBookCommand(this));
+        
         // Register listeners
-        getServer().getPluginManager().registerEvents(new ProtectionListener(timerManager), this);
-        getServer().getPluginManager().registerEvents(new BookListener(bookManager, particleManager), this);
-
-        getLogger().info("SMPStarter enabled! Waiting for the SMP to start.");
+        getServer().getPluginManager().registerEvents(new BookListener(this), this);
+        getServer().getPluginManager().registerEvents(new ProtectionListener(this), this);
+        
+        getLogger().info("SMPStarter has been enabled!");
     }
-
+    
     @Override
     public void onDisable() {
-        getLogger().info("SMPStarter disabled!");
+        getLogger().info("SMPStarter has been disabled!");
+    }
+    
+    public static SMPStarter getInstance() {
+        return instance;
+    }
+    
+    public TimerManager getTimerManager() {
+        return timerManager;
+    }
+    
+    public BookManager getBookManager() {
+        return bookManager;
     }
 }
