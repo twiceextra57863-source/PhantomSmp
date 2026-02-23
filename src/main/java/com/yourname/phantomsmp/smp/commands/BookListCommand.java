28382e0ae5ce6b36e5fd85b13e4d8f110c6bd49b
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-24 02:06:59 +0530

    Create BookListCommand.java

---
+++ src/main/java/com/yourname/phantomsmp/smp/commands/BookListCommand.java
@@ -0,0 +1,32 @@
+package com.phantom.smp.commands;
+
+import com.phantom.smp.PhantomSMP;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class BookListCommand implements CommandExecutor {
+    
+    private final PhantomSMP plugin;
+    
+    public BookListCommand(PhantomSMP plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§cOnly players can use this command!");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        // Open the book GUI
+        plugin.getGuiManager().openBookGUI(player);
+        
+        return true;
+    }
+}
