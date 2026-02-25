9db582caa0cd2862c7e5ee4d31a80ac8c1f5da86
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-26 00:05:58 +0530

    Create TradeDenyCommand.java

---
+++ src/main/java/com/yourname/phantomsmp/smp/commands/TradeDenyCommand.java
@@ -0,0 +1,31 @@
+// TradeDenyCommand.java
+package com.phantom.smp.commands;
+
+import com.phantom.smp.PhantomSMP;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TradeDenyCommand implements CommandExecutor {
+    
+    private final PhantomSMP plugin;
+    
+    public TradeDenyCommand(PhantomSMP plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§cOnly players can use this command!");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        plugin.getTradeManager().denyRequest(player);
+        
+        return true;
+    }
+}
