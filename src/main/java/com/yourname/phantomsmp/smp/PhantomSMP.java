06a7cbe7874ab38b53618f097852e5a75bb877d5
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-20 22:37:18 +0530

    Update PhantomSMP.java

--- src/main/java/com/yourname/phantomsmp/smp/PhantomSMP.java
+++ src/main/java/com/yourname/phantomsmp/smp/PhantomSMP.java
@@ -10,129 +10,62 @@ public class PhantomSMP extends JavaPlugin {
     private static PhantomSMP instance;
     
     // Managers
-    private DataManager dataManager;
     private GemManager gemManager;
     private CooldownManager cooldownManager;
-    private TradeManager tradeManager;
     private GraceManager graceManager;
-    private ParticleManager particleManager;
-    private AnimationManager animationManager;
     
     @Override
     public void onEnable() {
         instance = this;
         
-        // Save default config
         saveDefaultConfig();
         
-        try {
-            // Initialize managers
-            getLogger().info("§7Initializing managers...");
-            this.dataManager = new DataManager(this);
-            this.gemManager = new GemManager(this);
-            this.cooldownManager = new CooldownManager(this);
-            this.tradeManager = new TradeManager(this);
-            this.graceManager = new GraceManager(this);
-            this.particleManager = new ParticleManager(this);
-            this.animationManager = new AnimationManager(this);
-            
-            // Register commands
-            registerCommands();
-            
-            // Register listeners
-            registerListeners();
-            
-            // Load all player data
-            if (dataManager != null) {
-                dataManager.loadAll();
-            }
-            
-            getLogger().info("§a╔════════════════════════════════════╗");
-            getLogger().info("§a║     PhantomSMP v" + getDescription().getVersion() + " Enabled      ║");
-            getLogger().info("§a║        Author: MineTwice           ║");
-            getLogger().info("§a║      Paper 1.21.4 Compatible       ║");
-            getLogger().info("§a╚════════════════════════════════════╝");
-            
-        } catch (Exception e) {
-            getLogger().severe("§cFailed to enable PhantomSMP: " + e.getMessage());
-            e.printStackTrace();
-            getServer().getPluginManager().disablePlugin(this);
-        }
+        getLogger().info("§a╔════════════════════════════════════╗");
+        getLogger().info("§a║     PhantomSMP v" + getDescription().getVersion() + " Enabled      ║");
+        getLogger().info("§a║        Author: MineTwice           ║");
+        getLogger().info("§a║      Paper 1.21.4 Compatible       ║");
+        getLogger().info("§a╚════════════════════════════════════╝");
+        
+        // Initialize managers
+        this.gemManager = new GemManager(this);
+        this.cooldownManager = new CooldownManager(this);
+        this.graceManager = new GraceManager(this);
+        
+        // Register commands
+        registerCommands();
+        
+        // Register listeners
+        registerListeners();
     }
     
     @Override
     public void onDisable() {
-        if (dataManager != null) {
-            dataManager.saveAll();
-            dataManager.close();
-        }
-        
         getServer().getScheduler().cancelTasks(this);
-        getLogger().info("§cPhantomSMP disabled!");
+        getLogger().info("§cPhantomSMP Disabled!");
     }
     
     private void registerCommands() {
-        // Register commands only if they exist in plugin.yml
         if (getCommand("smpstart") != null)
             getCommand("smpstart").setExecutor(new SMPStartCommand(this));
         if (getCommand("smprewind") != null)
             getCommand("smprewind").setExecutor(new SMPRewindCommand(this));
-        if (getCommand("books") != null)
-            getCommand("books").setExecutor(new GemsCommand(this));
         if (getCommand("bookgive") != null)
-            getCommand("bookgive").setExecutor(new GemGiveCommand(this));
-        if (getCommand("trade") != null)
-            getCommand("trade").setExecutor(new TradeCommand(this));
+            getCommand("bookgive").setExecutor(new BookGiveCommand(this));
         if (getCommand("bookinfo") != null)
-            getCommand("bookinfo").setExecutor(new DebugCommands(this));
-        if (getCommand("setbooklevel") != null)
-            getCommand("setbooklevel").setExecutor(new DebugCommands(this));
-        if (getCommand("addkills") != null)
-            getCommand("addkills").setExecutor(new DebugCommands(this));
-        if (getCommand("resetcooldowns") != null)
-            getCommand("resetcooldowns").setExecutor(new DebugCommands(this));
+            getCommand("bookinfo").setExecutor(new BookInfoCommand(this));
     }
     
     private void registerListeners() {
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-        getServer().getPluginManager().registerEvents(new GemProtectionListener(this), this);
+        getServer().getPluginManager().registerEvents(new BookProtectionListener(this), this);
         getServer().getPluginManager().registerEvents(new AbilityListener(this), this);
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
-        getServer().getPluginManager().registerEvents(new TradeListener(this), this);
-        getServer().getPluginManager().registerEvents(new InventoryListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
     }
     
-    public static PhantomSMP getInstance() {
-        return instance;
-    }
-    
-    // Manager Getters
-    public DataManager getDataManager() {
-        return dataManager;
-    }
-    
-    public GemManager getGemManager() {
-        return gemManager;
-    }
-    
-    public CooldownManager getCooldownManager() {
-        return cooldownManager;
-    }
-    
-    public TradeManager getTradeManager() {
-        return tradeManager;
-    }
-    
-    public GraceManager getGraceManager() {
-        return graceManager;
-    }
-    
-    public ParticleManager getParticleManager() {
-        return particleManager;
-    }
-    
-    public AnimationManager getAnimationManager() {
-        return animationManager;
-    }
+    public static PhantomSMP getInstance() { return instance; }
+    public GemManager getGemManager() { return gemManager; }
+    public CooldownManager getCooldownManager() { return cooldownManager; }
+    public GraceManager getGraceManager() { return graceManager; }
 }
